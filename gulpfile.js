e22802c048d7ad91b29ac7e834a23887cde89357
Wawan <syzwnzhr@gmail.com>
2020-09-11 00:51:29 +0800

    migrating gulpfile to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -23,109 +23,112 @@ var folder = {
   build: 'build/'
 }
 
-// Image Processing
-gulp.task('images', function() {
+function images() {
   var out = folder.build + 'assets/images/';
-  return gulp.src(folder.src + 'assets/images/**/*')
-    .pipe(newer(out))
-    .pipe(imagemin({ optimizationLevel: 5 }))
-    .pipe(gulp.dest(out));
-})
+  return gulp
+  .src(folder.src + 'assets/images/**/*')
+  .pipe(newer(out))
+  .pipe(imagemin({ optimizationLevel: 5 }))
+  .pipe(gulp.dest(out));
+}
 
 // JS Processing
-gulp.task('js', function() {
+function js() {
   var jsbuild = gulp.src(folder.src + 'assets/js/**/*')
-    .pipe(deporder())
-    .pipe(concat('main.min.js'))
-    // .pipe(stripdebug())
-    .pipe(uglify());
-
+  .pipe(deporder())
+  .pipe(concat('main.min.js'))
+  .pipe(stripdebug())
+  .pipe(uglify());
   return jsbuild.pipe(gulp.dest(folder.build + 'assets/js/'));
-})
+}
+
 // Importing JQuery Popper and Bootstrap JS files
-gulp.task('importjs', function() {
+function importjs() {
   return gulp.src([
     './node_modules/jquery/dist/jquery.js',
     './node_modules/popper.js/dist/umd/popper.js', 
-    './node_modules/bootstrap/dist/js/bootstrap.js'
+    './node_modules/bootstrap/dist/js/bootstrap.js',
   ])
-  .pipe(rename(function(path){
+  .pipe(rename(function(path) {
     path.basename = path.basename + '.min';
-    path.extname  = '.js';
+    path.extname = '.js';
   }))
   .pipe(stripdebug())
   .pipe(uglify())
   .pipe(gulp.dest(folder.build + 'assets/js/'));
-})
+}
 
 // CSS Processing 
-gulp.task('css', ['images'], function() {
+function css() {
   var postCssOpts = [
     assets({ loadPaths: ['assets/images/'] }),
-    autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
-    mqpacker,
-    cssnano
+    mqpacker, cssnano
   ];
-
   return gulp.src(folder.src + 'assets/scss/main.scss')
-    .pipe(sass({
-      imagePath: 'assets/images/',
-      precision: 3,
-      errLogToConsole: true
-    }))
-    .pipe(postcss(postCssOpts))
-    .pipe(rename('main.min.css'))
-    .pipe(gulp.dest(folder.build + 'assets/css/'))
-    .pipe(browserSync.stream());
-})
+  .pipe(sass({
+    imagePaths: 'assets/images/',
+    precision: 3,
+    errLogToConsole: true,
+  }))
+  .pipe(postcss(postCssOpts))
+  .pipe(rename('main.min.css'))
+  .pipe(gulp.dest(folder.build + 'assets/css/'))
+  // .pipe(browserSync.stream());
+}
 
 // Importing Bootstrap SCSS
-gulp.task('bootstrap', function() {
+function bootstrap() {
   return gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(rename('bootstrap.min.css'))
-    .pipe(gulp.dest(folder.build + 'assets/css/'))
-    .pipe(browserSync.stream());
-})
+  .pipe(sass({
+    outputStyle: 'compressed',
+  }))
+  .pipe(rename('bootstrap.min.css'))
+  .pipe(gulp.dest(folder.build + 'assets/css/'))
+  .pipe(browserSync.stream());
+}
 
 // Watch for changes
-gulp.task('watch', function() {
+function watch() {
   browserSync.init({
-    server: "./build",
-    open: false
+    server: './build',
+    open: false,
   });
-  gulp.watch(folder.src + 'assets/images/**/*', ['images']).on('change', browserSync.reload);
-  gulp.watch(folder.src + 'assets/js/**/*', ['js']).on('change', browserSync.reload);
-  gulp.watch(folder.src + 'assets/scss/**/*', ['css']);
-  gulp.watch(folder.src + 'views/**/*', ['views']).on('change', browserSync.reload);
-})
+  gulp.watch(folder.src + 'assets/images/**/*', gulp.series(images)).on('change', browserSync.reload);
+  gulp.watch(folder.src + 'assets/js/**/*', gulp.series(js)).on('change', browserSync.reload);
+  gulp.watch(folder.src + 'assets/scss/**/*', gulp.series(css)).on('change', browserSync.reload);
+  gulp.watch(folder.src + 'views/**/*', gulp.series(views)).on('change', browserSync.reload);
+}
 
 // Pug Processing
-gulp.task('views', function buildHTML(){
+function views() {
   return gulp.src(folder.src + 'views/**/*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
-    //  Renaming subfolders
-    .pipe(rename(function(path){
-      path.dirname   = path.dirname;
-      path.basename  = 'index';
-      path.extname   = '.html';
-    }))
-    .pipe(htmlclean()) // Minifying HTML
-    .pipe(gulp.dest(folder.build));
-})
+  .pipe(pug({
+    pretty: true
+  }))
+  // renaming subfolders
+  .pipe(rename(function(path){
+    path.dirname  = path.dirname;
+    path.basename = 'index';
+    path.extname  = '.html';
+  }))
+  .pipe(htmlclean()) // html minifying
+  .pipe(gulp.dest(folder.build));
+}
 
 // Cleaning up the final build
-gulp.task('clean:build', function() {
-  return del.sync('build')
-})
-gulp.task('build', ['clean:build', 'run']);
+// function build() {
+//   return del.sync('build')
+// }
+// gulp.task('clean:build', function() {
+//   return del.sync('build')
+// })
+// gulp.task('build', ['clean:build', 'run']);
 
 // Run all tasks
-gulp.task('run', ['views', 'images', 'css', 'js', 'bootstrap', 'importjs']);
+// gulp.task('run', ['views', 'images', 'css', 'js', 'bootstrap', 'importjs']);
 
 // Default task
-gulp.task('default', ['run', 'watch']);
+// gulp.task('default', ['run', 'watch']);
+
+// gulp.task('default', gulp.series(gulp.parallel(views, images, css, js, bootstrap, importjs), watch));
+exports.default = gulp.series(gulp.parallel(views, images, css, js, bootstrap, importjs), watch);
